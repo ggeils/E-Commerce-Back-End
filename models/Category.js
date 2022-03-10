97009c8dbcc88bbc42bceeec938d58f25bb795ba
George Geils <95538913+ggeils@users.noreply.github.com>
2022-03-10 01:07:59 -0500

    defined columns in ProductTag.js and fixed spacing

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
@@ -8,22 +8,22 @@ Category.init(
   {
     // define columns
     id: {
-      type: DataTypes.INTEGER, 
-      allowNull : false, 
+      type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
