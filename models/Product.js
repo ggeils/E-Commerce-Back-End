68ce6ec72d41c70dfc9e0f9023d3138ed8a20f54
George Geils <95538913+ggeils@users.noreply.github.com>
2022-03-10 01:06:19 -0500

    defined columns in product.js and added comments for each

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // price column 
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    // stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    // category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
